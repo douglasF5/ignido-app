0e8b45c262d6d1ce56710c701e72bf27eda52fe8
Douglas Ferreira <dfaferreira46@gmail.com>
2023-02-15 10:34:58 -0300

    fix assets links issue

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:douglasF5/ignido-app.git main:gh-pages
+
+cd -
