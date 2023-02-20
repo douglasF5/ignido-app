7fcf07098f160d11c22e79d19622428b0da7149d
Douglas Ferreira <dfaferreira46@gmail.com>
2023-02-20 08:04:33 -0300

    fix assets links issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ignido-app/',
+  base: './',
   plugins: [react()],
   build: {
-    outDir: './dist/ignido-app/',
     assetsDir: './'
   }
 })
